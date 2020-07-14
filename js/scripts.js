fc6876af3747fa5219b6d6c460a524559bd79d7f
Isabella Chen <miss.izzyc@gmail.com>
2020-07-14 16:09:45 +0200

    Add owl carousel

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+(function ($) {
+  debugger;
+  $('.owl-carousel').owlCarousel();
+})(jQuery);
--- single.php
+++ single.php
@@ -16,18 +16,30 @@ get_header();
   <?php
   while (have_posts()) :
     the_post();
-
+    $post_country = get_the_tags()[0]->name; //name of country
     get_template_part('template-parts/content', get_post_type());
-    the_post_navigation(
-      array(
-        'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'travellers-field-guide') . '</span> <span class="nav-title">%title</span>',
-        'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'travellers-field-guide') . '</span> <span class="nav-title">%title</span>',
-      )
-    );
-
-  endwhile; // End of the loop.
+    // the_post_navigation(
+    //   array(
+    //     'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'travellers-field-guide') . '</span> <span class="nav-title">%title</span>',
+    //     'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'travellers-field-guide') . '</span> <span class="nav-title">%title</span>',
+    //   )
+    // );
   ?>
+  <?php endwhile; ?>
 
+  <div class="owl-carousel">
+    <?php
+    $the_query = new WP_Query(array('tag' => $post_country));
+    while ($the_query->have_posts()) : $the_query->the_post() ?>
+      <?php
+      if (has_post_thumbnail()) {
+        echo the_post_thumbnail('post-thumbnail', ['class' => 'owl-tile']);
+      }
+      ?>
+    <?php endwhile;
+    wp_reset_postdata();
+    ?>
+  </div>
 </main><!-- #main -->
 
 <?php
