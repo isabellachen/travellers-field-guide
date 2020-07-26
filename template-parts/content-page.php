1d28c2809a3d16eeee6568f271cdb3500cb35ef0
Isabella Chen <miss.izzyc@gmail.com>
2020-07-26 18:55:31 +0200

    fix the content page template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Template part for displaying page content in page.php
+ * Template part for displaying posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -10,16 +10,35 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?> single-post" <?php post_class(); ?>>
   <header class="entry-header">
-    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+    <!-- Subtitle: display the country category for the current post, e.g. "Iceland" -->
+    <div class="entry-header-subtitle container">
+    </div>
+    <div class="entry-header-title-wrapper">
+      <h1 class="entry-header-title"><?php the_title() ?></h1>
+    </div>
+    <div class="entry-header-excerpt-wrapper">
+      <div class="entry-header-excerpt entry-header-excerpt--mobile"><?php echo the_excerpt() ?></div>
+    </div>
   </header><!-- .entry-header -->
-
-  <?php travellers_field_guide_post_thumbnail(); ?>
 
   <div class="entry-content dropcap">
     <?php
-    the_content();
+    the_content(
+      sprintf(
+        wp_kses(
+          /* translators: %s: Name of current post. Only visible to screen readers */
+          __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'travellers-field-guide'),
+          array(
+            'span' => array(
+              'class' => array(),
+            ),
+          )
+        ),
+        wp_kses_post(get_the_title())
+      )
+    );
 
     wp_link_pages(
       array(
@@ -29,27 +48,5 @@
     );
     ?>
   </div><!-- .entry-content -->
-
-  <?php if (get_edit_post_link()) : ?>
-    <footer class="entry-footer">
-      <?php
-      edit_post_link(
-        sprintf(
-          wp_kses(
-            /* translators: %s: Name of current post. Only visible to screen readers */
-            __('Edit <span class="screen-reader-text">%s</span>', 'travellers-field-guide'),
-            array(
-              'span' => array(
-                'class' => array(),
-              ),
-            )
-          ),
-          wp_kses_post(get_the_title())
-        ),
-        '<span class="edit-link">',
-        '</span>'
-      );
-      ?>
-    </footer><!-- .entry-footer -->
-  <?php endif; ?>
+  <div class="related-posts-marker"></div>
 </article><!-- #post-<?php the_ID(); ?> -->
