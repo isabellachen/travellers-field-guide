62f0659142457fbb16309cd0d99bf61cde6e4a38
Isabella Chen <miss.izzyc@gmail.com>
2020-07-27 21:40:51 +0200

    seperate the js

--- js/scripts-single.js
+++ js/scripts-single.js
@@ -122,18 +122,8 @@
     });
   }
 
-  function scrollOnPaginate() {
-    const url = window.location.href;
-    const isPaginated = url.includes('page');
-    const $scrollTo = $('.frontpage-posts-wrapper');
-    if (isPaginated) {
-      window.scroll(0, $scrollTo.offset().top - 60);
-    }
-  }
-
   $(window).on('load', function () {
     initForLongContent();
-    scrollOnPaginate();
   });
 
   $(document).ready(function () {
