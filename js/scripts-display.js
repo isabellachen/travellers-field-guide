5476b6dacabaa9611d878851943c565e62dc9db4
Isabella Chen <miss.izzyc@gmail.com>
2020-08-07 12:25:30 +0200

    scroll up when read less

--- js/scripts-display.js
+++ js/scripts-display.js
@@ -8,14 +8,34 @@
     }
   }
 
+  function elInViewport(el) {
+    if (typeof jQuery === 'function' && el instanceof jQuery) {
+      el = el[0];
+    }
+
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
   function archiveReadMore() {
     const $button = $('.archive-read-button');
     const $lead = $('.archive-lead-content');
+    const $archiveLead = $('.archive-lead');
 
     $button.click(() => {
       $lead.toggleClass('long');
       $button.toggleClass('archive-read-button--more');
       $button.toggleClass('archive-read-button--less');
+
+      if (!elInViewport($archiveLead)) {
+        window.scroll(0, $archiveLead.offset().top - 50);
+      }
     });
   }
 
